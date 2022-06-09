298ccd5b58538c6c07167d517d5ad3e629b4a1e0
jchyczynski <jasiek.ch16@gmail.com>
2022-06-10 01:35:22 +0200

    removed default ufo map markers

--- signalFinder.py
+++ signalFinder.py
@@ -1,3 +1,5 @@
+import threading
+
 from kivy.clock import Clock
 from kivy.garden.mapview import MapView, MapMarkerPopup, MapMarker
 from kivy.lang import Builder
@@ -21,11 +23,16 @@ class SignalFinder(BoxLayout):
     def init_ui(self, dt=0):
         self.ids.map_view.add_layer(self.heatmap_layer, mode='scatter')
 
-    def calculate_ratio(self):
+    def calculate_ratio(self, dt=0):
         prev = self.ids.slider.value
         self.ids.slider.value = self.probability_calculator.get_best_ratio()
         if prev != self.ids.slider.value:
             self.heatmap_layer.reposition()
+
+    def threaded_calculate_ratio(self):
+        # Clock.schedule_once(self.calculate_ratio, 0)
+        thread = RatioCalculateThread(self)
+        thread.start()
 
     def enter_coordinates(self):
         self.save_popup.open()
@@ -40,3 +47,12 @@ class SignalFinder(BoxLayout):
         marker = MapMarker(lat=lat, lon=lon, source='alien100.png', size=(1, 0.1), size_hint=(None, None),
                            allow_stretch=True)
         self.ids.map_view.add_marker(marker)
+
+
+class RatioCalculateThread(threading.Thread):
+    def __init__(self, signal_finder: SignalFinder):
+        super().__init__()
+        self.signal_finder = signal_finder
+
+    def run(self) -> None:
+        self.signal_finder.calculate_ratio()
